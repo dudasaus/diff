812da9dcd6b47bda5712b6b940cef26296dd1381
Austin Dudas <dudas.austin@gmail.com>
2026-05-09 10:07:54 -0600

    edit dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import { Diff } from "./components/diff/Diff";
+import type { FileContents } from "@pierre/diffs";
+import { FileContentDialog } from "./components/FileContentDialog/FileContentDialog";
+import { oldFileAtom } from "./atoms";
+import type { PrimitiveAtom } from "jotai";
 
 export default function App() {
+  const [editingFile, setEditingFile] = useState<PrimitiveAtom<FileContents> | null>(null);
+
+
   return <>
+    {editingFile ? <FileContentDialog 
+      fileContentAtom={editingFile} 
+      handleClose={() => { setEditingFile(null) }} 
+    /> : <button 
+    onClick={() => setEditingFile(oldFileAtom)}
+  >Edit</button>}
+
     <Diff/>
   </>
 }
---
+++ src/components/FileContentDialog/FileContentDialog.tsx
@@ -0,0 +1,38 @@
+import './FileContentDialog.css';
+import type { FileContents } from "@pierre/diffs";
+import type { PrimitiveAtom } from 'jotai';
+import { useAtom } from "jotai";
+import { useEffect, useId, useRef } from "react";
+
+export function FileContentDialog(props: {
+  fileContentAtom: PrimitiveAtom<FileContents>,
+  handleClose: () => void
+}) {
+  const dialogRef = useRef<HTMLDialogElement>(null)
+  const titleInputRef = useRef<HTMLInputElement>(null);
+  const contentInputRef = useRef<HTMLTextAreaElement>(null);
+  const titleId = useId();
+  const contentId = useId();
+  const [fileContent, setFileContent] = useAtom(props.fileContentAtom);
+
+  useEffect(() => {
+    dialogRef.current?.showModal();
+  }, [])
+
+  return <dialog ref={dialogRef} className="file-content-dialog">
+    <form onSubmit={(ev) => {
+      ev.preventDefault();
+      setFileContent({
+        name: titleInputRef.current?.value.trim() ?? '',
+        contents: contentInputRef.current?.value ?? '',
+      });
+      props.handleClose();
+    }}>
+      <label htmlFor={titleId}>File title</label>
+      <input id={titleId} defaultValue={fileContent.name} ref={titleInputRef} />
+      <label htmlFor={contentId}>Content</label>
+      <textarea id={contentId} defaultValue={fileContent.contents} ref={contentInputRef}/>
+      <button type="submit">Save</button>
+    </form>
+  </dialog>
+}
