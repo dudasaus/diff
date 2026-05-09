57b03ab40aa78b93cc6fc9f6da1b4a4487d09645
Austin Dudas <dudas.austin@gmail.com>
2026-05-09 16:05:03 -0600

    local storage

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,18 +1,18 @@
 import type { FileContents } from "@pierre/diffs";
-import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
 export type DiffStyle = "split" | "unified";
 
-export const diffStyleAtom = atom<DiffStyle>("split");
+export const diffStyleAtom = atomWithStorage<DiffStyle>("diffStyle", "split");
 
-export const fileSuffixAtom = atom<string>("txt");
+export const fileSuffixAtom = atomWithStorage<string>("fileSuffix", "txt");
 
-export const oldFileAtom = atom<FileContents>({
+export const oldFileAtom = atomWithStorage<FileContents>("oldFile", {
   name: "base",
   contents: "Hello world!\n",
 });
 
-export const newFileAtom = atom<FileContents>({
+export const newFileAtom = atomWithStorage<FileContents>("newFile", {
   name: "new",
   contents: "Hello friends!\n",
 });
