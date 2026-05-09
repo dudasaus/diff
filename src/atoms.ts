c077aceb2194a8c1713b4484661556001e77db63
Austin Dudas <dudas.austin@gmail.com>
2026-05-09 09:26:49 -0600

    atoms

---
+++ src/atoms.ts
@@ -0,0 +1,12 @@
+import type { FileContents } from "@pierre/diffs";
+import { atom } from "jotai";
+
+export const oldFileAtom = atom<FileContents>({
+  name: "base.txt",
+  contents: "Hello world!\n",
+});
+
+export const newFileAtom = atom<FileContents>({
+  name: "new.txt",
+  contents: "Hello friends!\n",
+});
--- src/components/diff/Diff.tsx
+++ src/components/diff/Diff.tsx
@@ -1,36 +1,17 @@
 import {
-  type FileContents,
   MultiFileDiff,
 } from '@pierre/diffs/react';
+import { useAtomValue } from 'jotai';
 import { useState } from 'react';
-
-const oldFile: FileContents = {
-  name: 'base.txt',
-  contents: `const std = @import("std");
-
-pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hi you, {s}!\\\\n", .{"world"});
-}
-`,
-};
-
-const newFile: FileContents = {
-  name: 'new.txt',
-  contents: `const std = @import("std");
-
-pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello there, {s}!\\\\n", .{"zig"});
-}
-`,
-};
+import { newFileAtom, oldFileAtom } from '../../atoms';
 
 type DiffStyle = "split" | "unified";
 
 export function Diff() {
 
   const [diffStyle, setDiffStyle] = useState<DiffStyle>('unified');
+  const oldFile = useAtomValue(oldFileAtom);
+  const newFile = useAtomValue(newFileAtom);
 
   return (<>
     <button onClick={() => setDiffStyle(diffStyle === 'split' ? 'unified' : 'split')}>
